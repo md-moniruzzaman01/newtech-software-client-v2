4172c29b0706928797d24f914f246548eca21999
fahim-khandakar <fahimkhandakar01@gmail.com>
2024-03-28 16:03:53 +0600

    change somthing

--- src/modules/warranty/complaints/ComplaintAddForWarranty/helpers/submitData.ts
+++ src/modules/warranty/complaints/ComplaintAddForWarranty/helpers/submitData.ts
@@ -7,15 +7,35 @@ import { authKey } from "../../../../../shared/config/constaints";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const handleDataSubmit = async (
-  addComplaint: any,
+  setIsLoading: any,
   fullData: any,
   setWarrantyAddedItem: any,
   setPartnerInfo: any,
   setIsNewPartner: any
 ) => {
   const token = getFromLocalStorage(authKey);
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      ...(token && { authorization: token }), // Conditionally set the authorization header
+    },
+    body: JSON.stringify(fullData),
+  };
+
   try {
-    const result = await addComplaint({ fullData, token });
+    setIsLoading(true);
+    const response = await fetch(
+      "http://16.16.166.48:5000/api/v2/complaints/create",
+      options
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to add complaint");
+    }
+
+    const result = await response.json();
 
     if ("data" in result) {
       setWarrantyAddedItem([]);
@@ -36,7 +56,9 @@ export const handleDataSubmit = async (
         icon: "error",
       });
     }
+    setIsLoading(false);
   } catch (error) {
     console.error("Error adding complaint:", error);
+    setIsLoading(false);
   }
 };
