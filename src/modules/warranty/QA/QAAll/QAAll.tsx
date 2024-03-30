8711db02160590e25c211860e6ca9532f063e2f7
fahim-khandakar <fahimkhandakar01@gmail.com>
2024-03-30 16:38:01 +0600

    change qa all

--- src/modules/warranty/QA/QAAll/QAAll.tsx
+++ src/modules/warranty/QA/QAAll/QAAll.tsx
@@ -1,28 +1,75 @@
+import { useState } from "react";
+import LoadingPage from "../../../../common/components/LoadingPage/LoadingPage";
 import SearchBar from "../../../../common/components/SearchBar/SearchBar";
 import StatusGroup from "../../../../common/components/Status Group";
-import Table from "../../../../common/components/Table/Table";
 import Navbar from "../../../../common/widgets/Navbar/Navbar";
 import Pagination from "../../../../common/widgets/Pagination/Pagination";
-import { DemoTableHeaderView, DemoTableValue } from "../../../../shared/config/constaints";
 
+import { authKey } from "../../../../shared/config/constaints";
+import { getFromLocalStorage } from "../../../../shared/helpers/local_storage";
 
-const QAAll = () => {
+import { useGetQcsQuery } from "../../../../redux/features/api/qc";
+import QAAllTable from "./partials/QAAllTable";
+import { QATableHeader } from "./config/constants";
+
+const QCMyItems = () => {
+  const [checkedRows, setCheckedRows] = useState<string[]>([]);
+  const token = getFromLocalStorage(authKey);
+  const id = "65f7d1b8ff0aba99b376d459";
+  const { data, isError, isLoading } = useGetQcsQuery({
+    id,
+    token,
+  });
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+  if (isError) {
+    console.error(isError);
+
+    return <div>Error</div>;
+  }
+
+  const handleCheckboxChange = (index: string) => {
+    if (checkedRows.includes(index)) {
+      setCheckedRows(checkedRows.filter((item) => item !== index));
+    } else {
+      setCheckedRows([...checkedRows, index]);
+    }
+  };
+  const handleAllCheckboxChange = () => {
+    if (checkedRows.length === data?.data?.length) {
+      // If all checkboxes are already checked, uncheck them all
+      setCheckedRows([]);
+    } else {
+      const allIds =
+        data?.data
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          ?.map((item: any) => item?._id)
+          .filter((id: string) => id !== undefined) || [];
+      if (allIds.length > 0) {
+        setCheckedRows(allIds as string[]);
+      }
+    }
+  };
+  console.log(data);
   return (
     <div className=" px-5">
-      <Navbar name="QA All" />
+      <Navbar name="QA All"></Navbar>
       <div className="pt-5">
         <SearchBar />
       </div>
       <div className="mt-5 p-3 bg-solidWhite">
         <div>
-          <StatusGroup btnGroupValue={[]}/>
+          <StatusGroup />
           <div className="pt-5">
-            <Table
-              view
-              Link="/complaints/order-details"
-              itemData={DemoTableValue}
-              HeaderData={DemoTableHeaderView}
-            ></Table>
+            <QAAllTable
+              Link="/qc/order-details"
+              itemData={data?.data}
+              HeaderData={QATableHeader}
+              checkedRows={checkedRows}
+              handleCheckboxChange={handleCheckboxChange}
+              handleAllCheckboxChange={handleAllCheckboxChange}
+            />
           </div>
         </div>
         <div className="absolute bottom-2 right-[50px]">
@@ -33,4 +80,4 @@ const QAAll = () => {
   );
 };
 
-export default QAAll;
+export default QCMyItems;
