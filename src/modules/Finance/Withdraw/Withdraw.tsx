64d30b0807fd95c74bfd3972daa77421a10519c8
fahim-khandakar <fahimkhandakar01@gmail.com>
2024-04-25 17:40:35 +0600

    update branch withdraw page

--- src/modules/Finance/Withdraw/Withdraw.tsx
+++ src/modules/Finance/Withdraw/Withdraw.tsx
@@ -1,5 +1,9 @@
 import BranchCommonHeader from "../../../common/components/BranchCommonHeader/BranchCommonHeader";
-import { FilterOptions, authKey } from "../../../shared/config/constaints";
+import {
+  FilterOptions,
+  authKey,
+  branches,
+} from "../../../shared/config/constaints";
 import RepairCompleteCard from "../../../common/components/RepairCompleteCard/RepairCompleteCard";
 import Pagination from "../../../common/widgets/Pagination/Pagination";
 import Button from "../../../common/components/Button";
@@ -8,6 +12,7 @@ import InputFilter from "../../../common/components/InputFilter/InputFilter";
 import { getFromLocalStorage } from "../../../shared/helpers/local_storage";
 import {
   useCreateWithdrawMutation,
+  useGetTotalAmountQuery,
   useGetWithdrawQuery,
 } from "../../../redux/features/api/withdraw";
 import CommonTable from "../../../common/components/Common Table/CommonTable";
@@ -15,11 +20,15 @@ import { TableHeader, tableLayout, withdrawOption } from "./config/constants";
 import { useEffect, useState } from "react";
 import LoadingPage from "../../../common/components/LoadingPage/LoadingPage";
 import swal from "sweetalert";
+import ErrorShow from "../../../common/components/Error Show/ErrorShow";
 
 const Withdraw = () => {
   const token = getFromLocalStorage(authKey);
-  const [createWithdraw] = useCreateWithdrawMutation();
+  const [createWithdraw, { isError, isSuccess, error }] =
+    useCreateWithdrawMutation();
+
   const { data, isLoading } = useGetWithdrawQuery({ token });
+  const { data: totalAmount } = useGetTotalAmountQuery({ token });
   const [currentPage, setCurrentPage] = useState(1);
   const [totalItems, setTotalItems] = useState(50);
   const limit = 10;
@@ -30,10 +39,12 @@ const Withdraw = () => {
       setTotalItems(data?.meta?.total);
     }
   }, [data, currentPage]);
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  // console.log(totalAmount);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const form = e.currentTarget;
+    const branch = (form.elements.namedItem("branch") as HTMLInputElement)
+      ?.value;
     const withdraw = (form.elements.namedItem("withdraw") as HTMLInputElement)
       ?.value;
     const amount = parseInt(
@@ -43,15 +54,16 @@ const Withdraw = () => {
     const note = (form.elements.namedItem("note") as HTMLInputElement)?.value;
 
     const fullData = {
+      branch,
       withdraw,
       amount,
       note,
     };
 
-    console.log(fullData);
+    // console.log(fullData);
 
-    const result = createWithdraw({ fullData, token });
-
+    const result = await createWithdraw({ fullData, token });
+    console.log("result", result);
     if ("data" in result) {
       swal("Withdraw successful.", {
         icon: "success",
@@ -67,6 +79,10 @@ const Withdraw = () => {
   if (isLoading) {
     return <LoadingPage />;
   }
+  if (isError) {
+    return <ErrorShow error={error} />;
+  }
+
   return (
     <div className=" px-5">
       <BranchCommonHeader
@@ -107,22 +123,36 @@ const Withdraw = () => {
               />
             </div>
           </div>
-          <div className="col-span-1 bg-[#FBFBFB] rounded-md text-center">
-            <h2 className="text-xl font-medium  py-5">Withdraw Method</h2>
-            <hr className="mx-10" />
-            <h2 className="font-bold text-xl pt-5 pb-2">$ 5,0000</h2>
-            <p>Show Branch Amount</p>
+          <div className="col-span-1 bg-[#FBFBFB] rounded-md">
+            <div className=" text-center">
+              <h2 className="text-xl font-medium  py-5">Withdraw Method</h2>
+              <hr className="mx-10" />
+              <h2 className="font-bold text-xl pt-5 pb-2">$ 5,0000</h2>
+              <p>Show Branch Amount</p>
+            </div>
             <div className="  my-5 w-10/12 mx-auto">
               <form className="space-y-3" onSubmit={handleSubmit}>
                 <InputFilter
                   required
+                  label="Branch"
+                  Filter={branches}
+                  inputName="branch"
+                />
+                <InputFilter
+                  required
+                  label="Type "
                   Filter={withdrawOption}
                   inputName="withdraw"
                 />
 
                 {/* Regular input components */}
-                <Input required inputName="amount" inputType="number" />
-                <Input inputName="note" />
+                <Input
+                  labelName="Amount"
+                  required
+                  inputName="amount"
+                  inputType="number"
+                />
+                <Input labelName="Note" inputName="note" />
                 <div>
                   <Button
                     type="submit"
