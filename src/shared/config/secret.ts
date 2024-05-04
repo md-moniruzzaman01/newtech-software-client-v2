e1df95ef0f738b0c501413e91d344adcbe290115
fahim-khandakar <fahimkhandakar01@gmail.com>
2024-05-04 15:14:50 +0600

    change in qa

--- src/shared/config/secret.ts
+++ src/shared/config/secret.ts
@@ -1,4 +1,5 @@
-export const SERVER_URL = "http://192.168.0.103:5000/api/v2";
+export const SERVER_URL = "https://nt.necgroupbd.net/api/v2";
 
 // https://nt.necgroupbd.net/api/v2
-// https://nt.necgroupbd.net/api/v2
+
+// http://192.168.0.103:5000/api/v2
