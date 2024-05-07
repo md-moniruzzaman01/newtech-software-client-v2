1cfe31329fc8c44190f2f55d98b4f37fc3d5391c
fahim-khandakar <fahimkhandakar01@gmail.com>
2024-05-07 15:34:55 +0600

    change somethings

--- src/modules/warranty/QC/ComplaintOrderDetailsQC/config/constants.ts
+++ src/modules/warranty/QC/ComplaintOrderDetailsQC/config/constants.ts
@@ -1,8 +1,6 @@
 export const repairStatus = [
   { id: "1", value: "QC" },
   { id: "2", value: "QC Ok" },
-  { id: "3", value: "Reject" },
-  { id: "4", value: "CID" },
-  { id: "5", value: "Not Repairable" },
-  { id: "6", value: "QC Failed" },
+  { id: "3", value: "CID" },
+  { id: "4", value: "QC Failed" },
 ];
--- src/shared/config/secret.ts
+++ src/shared/config/secret.ts
@@ -1,4 +1,4 @@
-export const SERVER_URL = "https://nt.necgroupbd.net/api/v2";
+// export const SERVER_URL = "https://nt.necgroupbd.net/api/v2";
+export const SERVER_URL = "http://192.168.0.2:5000/api/v2";
 
 // https://nt.necgroupbd.net/api/v2
-// http://192.168.0.2:5000/api/v2
