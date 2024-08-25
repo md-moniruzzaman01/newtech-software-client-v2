07593f7915f8cacf03383a1fffacd6b24d788a63
Md Moniruzzaman <md.moniruzzaman1050@gmail.com>
2024-08-25 15:12:02 +0600

    updating admin dashboard

--- src/shared/config/secret.ts
+++ src/shared/config/secret.ts
@@ -1,4 +1,4 @@
-export const SERVER_URL = import.meta.env.VITE_SOME_KEY;
-// export const SERVER_URL = "http://192.168.0.105:5000/api/v2";
+
+export const SERVER_URL = "http://localhost:5001/api/v2";
 
 //http://localhost:5000/api/v2
