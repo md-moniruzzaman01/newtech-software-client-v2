4f8162f16d35c7437dd2f6c66f8040e681e1437a
fahim-khandakar <fahimkhandakar01@gmail.com>
2024-10-24 13:08:33 +0600

    details ok

--- src/modules/Utilitis/Engineer/Engineer Details Page/partials/UserInfo.tsx
+++ src/modules/Utilitis/Engineer/Engineer Details Page/partials/UserInfo.tsx
@@ -17,103 +17,147 @@ import {
   tableLayoutForQc,
   tableLayoutForRepair,
 } from "../config/constants";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+import { getUserInfo } from "../../../../../services/auth.service";
 
 const UserInfo = ({ data }) => {
+  const userInfo = getUserInfo();
   const { user, QaItems, QcItems, repairItems, discountItems, BillItems } =
     data.data;
-  const { Engineer, needsPasswordChange } = user;
-
+  const { Engineer } = user;
+  console.log(data, userInfo);
   return (
     <div>
-      <div className="bg-white pt-2 px-5 pb-3 rounded-lg shadow-lg  mx-auto">
-        <div className="flex items-center justify-between py-5">
-          <header className="flex justify-between items-center ">
+      <div className="bg-white px-5 pb-3 rounded-lg shadow-lg  mx-auto">
+        <div className="flex items-center justify-between pt-5">
+          {/* <header className="flex justify-between items-center ">
             <h1 className="text-3xl font-bold flex items-center">
               <FontAwesomeIcon icon={faUser} className="mr-2 text-blue-500" />
               Employee's Summary
             </h1>
-          </header>
-          {/* Password Change Action */}
-          {needsPasswordChange && (
-            <div className="">
-              <NavLink
-                to={`${
-                  user?.role === "admin"
-                    ? "/change-password"
-                    : "/user-change-password"
-                }`}
-              >
-                <Button small>Change Password</Button>
-              </NavLink>
-            </div>
-          )}
+          </header> */}
         </div>
+
         {/* Profile Header */}
-        <div className="flex flex-col text-center items-center mb-6">
-          {Engineer?.profileImage ? (
-            <img
-              src={Engineer?.profileImage}
-              alt="Profile"
-              className="w-24 h-24 rounded-full border-2 border-blue-500 shadow-lg"
-            />
-          ) : (
-            <div className="w-24 h-24 rounded-full border-2 border-blue-500 flex items-center justify-center text-4xl shadow-lg">
-              {icons?.user}
+        <div className="">
+          <div className="grid grid-cols-6 gap-5">
+            {/* image  */}
+            <div className="col-span-1">
+              {Engineer?.profileImage ? (
+                <img
+                  src={Engineer?.profileImage}
+                  alt="Profile"
+                  className="w-52 h-40  border-2 border-blue-500 shadow-lg"
+                />
+              ) : (
+                <div className="w-40 h-40  border-2 border-blue-500 flex items-center justify-center text-4xl shadow-lg">
+                  {icons?.user}
+                </div>
+              )}
             </div>
-          )}
-          <h1 className="text-2xl font-bold mt-3 text-gray-800">
-            {Engineer?.name?.firstName} {Engineer?.name?.lastName}
-          </h1>
-          <p className="text-gray-600 text-sm">{Engineer?.designation}</p>
-          <p className="text-gray-700 text-sm">{Engineer?.email}</p>
-        </div>
 
-        {/* User and Engineer Info */}
-        <div className="flex justify-around mb-4 bg-gray-50 p-4 rounded-lg shadow-sm text-sm">
-          <div className="space-y-1">
-            <h2 className="font-semibold text-gray-800">User Info</h2>
-            <p className="text-gray-700">
-              <span className="font-medium">ID:</span> {user?.id}
-            </p>
-            <p className="text-gray-700">
-              <span className="font-medium">Role:</span> {user?.role}
-            </p>
-            <p className="text-gray-700">
-              <span className="font-medium">Branch:</span>{" "}
-              {getBranchName(user?.branch)}
-            </p>
+            <div className="col-span-4">
+              <h1 className="text-4xl font-bold py-2 text-gray-800">
+                {Engineer?.name?.firstName} {Engineer?.name?.lastName}
+              </h1>
+              <p className="text-xl text-blue-500 pb-4 font-medium">
+                {Engineer?.designation}
+              </p>
 
-            <p className="text-gray-700">
-              <span className="font-medium">Contact:</span>{" "}
-              {Engineer?.contactNo}
-            </p>
-            <p className="text-gray-700">
-              <span className="font-medium">Created:</span>{" "}
-              {new Date(Engineer?.createdAt).toLocaleDateString()}
-            </p>
-            <p className="text-gray-700">
-              <span className="font-medium">Updated:</span>{" "}
-              {new Date(Engineer?.updatedAt).toLocaleDateString()}
-            </p>
-          </div>
+              <div className="pt-2">
+                {userInfo?.userId === user?.id || userInfo?.role === "admin" ? (
+                  <NavLink
+                    to={`${
+                      user?.role === "admin"
+                        ? "/change-password"
+                        : "/user-change-password"
+                    }`}
+                  >
+                    <Button small>Change Password</Button>
+                  </NavLink>
+                ) : (
+                  <abbr title="You are not authorized to change the passwords of other users.">
+                    <Button small>Change Password</Button>
+                  </abbr>
+                )}
+              </div>
+            </div>
 
-          <div className="">
-            <h2 className="font-semibold text-gray-800">Power Levels</h2>
-            <ul className="list-disc pl-5 text-gray-600">
-              {Engineer?.power?.map((power, index) => (
-                <li key={index}>{getPowerName(power)}</li>
-              ))}
-            </ul>
-          </div>
-          <div className="">
-            <h2 className="font-semibold text-gray-800">Skills ID</h2>
-            <ul className="list-disc pl-5 text-gray-600">
-              {Engineer?.Skill?.map((skill, index) => (
-                <li key={index}>{skill}</li>
-              ))}
-            </ul>
+            <div className="italic col-span-1">
+              <strong>Join Date: </strong>
+              <span className="text-blue-500 ">
+                {Engineer?.createdAt?.slice(0, 10)}
+              </span>
+            </div>
+
+            {/* powers  */}
+            <div className="pl-3 col-span-1 space-y-5">
+              <div>
+                <h2 className="font-semibold text-blue-500 text-lg">
+                  Power Levels
+                </h2>
+                <ul className="list-disc pl-5 text-gray-600">
+                  {Engineer?.power?.map((power, index) => (
+                    <li key={index}>{getPowerName(power)}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h2 className="font-semibold text-blue-500 text-lg">
+                  Skills ID
+                </h2>
+                <ul className="list-disc pl-5 text-gray-600">
+                  {Engineer?.Skill?.map((skill, index) => (
+                    <li key={index}>{skill}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            {/* User and Engineer Info */}
+            <div className="col-span-5 ">
+              <h2 className="font-semibold text-blue-500 text-lg border-b border-grayForBorder pb-2">
+                User Info
+              </h2>
+              <div className="pt-2">
+                <div className="grid grid-cols-3">
+                  <div className="col-span-2 space-y-2 text-lg">
+                    <p className="text-gray-700 border-b border-grayForBorder pb-2">
+                      <span className="font-semibold">ID:</span>
+                    </p>
+                    <p className="text-gray-700 border-b border-grayForBorder pb-2">
+                      <span className="font-semibold">Role:</span>
+                    </p>
+                    <p className="text-gray-700 border-b border-grayForBorder pb-2">
+                      <span className="font-semibold">Branch:</span>
+                    </p>
+                    <p className="text-gray-700 border-b border-grayForBorder pb-2">
+                      <span className="font-semibold">Contact:</span>
+                    </p>
+                    <p className="text-gray-700 border-b border-grayForBorder pb-2">
+                      <span className="font-semibold">Updated:</span>
+                    </p>
+                  </div>
+                  <div className="col-span-1 space-y-2 text-blue-500 text-lg">
+                    <p className="border-b border-grayForBorder pb-2">
+                      {user?.id}
+                    </p>
+                    <p className="border-b border-grayForBorder pb-2">
+                      {user?.role}
+                    </p>
+                    <p className="border-b border-grayForBorder pb-2">
+                      {getBranchName(user?.branch)}
+                    </p>
+                    <p className="border-b border-grayForBorder pb-2">
+                      {Engineer?.contactNo}
+                    </p>
+                    <p className="border-b border-grayForBorder pb-2">
+                      {Engineer?.updatedAt?.slice(0, 10)}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
